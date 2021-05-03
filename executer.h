14bb5d104262e57840f4eea0c5db2cab6214ce31
antony-frolov <i.am.antonyfrolov@gmail.com>
2021-05-03 11:27:03 +0300

    Added string support, everything kinda working, except structs

---
+++ executer.h
@@ -0,0 +1,233 @@
+#ifndef MLANG_INTERPRETER_EXECUTER_H
+#define MLANG_INTERPRETER_EXECUTER_H
+
+class Executer {
+public:
+    void execute(vector<Lex>& poliz);
+};
+
+void Executer::execute(vector<Lex>& poliz) {
+    Lex pc_el;
+    stack<int> args;
+    int i, j, index = 0, size = poliz.size();
+    while (index < size) {
+        pc_el = poliz[index];
+        cout << index << endl;
+        switch (pc_el.get_type()) {
+            case LEX_TRUE: case LEX_FALSE: case LEX_NUM: case LEX_STR: case POLIZ_ADDRESS: case POLIZ_LABEL:
+                args.push(pc_el.get_value());
+                break;
+            case LEX_ID:
+                i = pc_el.get_value();
+                if (TID[i].get_assign()) {
+                    args.push(TID[i].get_value());
+                    break;
+                } else {
+                    throw "POLIZ: indefinite identifier";
+                }
+
+            case LEX_NOT:
+                from_st(args, i);
+                args.push(!i);
+                break;
+
+            case LEX_OR:
+                from_st (args, i);
+                from_st (args, j);
+                args.push (j || i);
+                break;
+
+            case LEX_AND:
+                from_st (args, i);
+                from_st (args, j);
+                args.push (j && i);
+                break;
+
+            case POLIZ_GO:
+                from_st(args, i);
+                index = i - 1;
+                break;
+
+            case POLIZ_FGO:
+                from_st (args, i);
+                from_st (args, j);
+                if (!j) index = i - 1;
+                break;
+
+            case LEX_WRITE:
+                from_st(args, j);
+                cout << j << endl;
+                break;
+
+            case LEX_SWRITE:
+                from_st(args, j);
+                cout << TSTR[j] << endl;
+                break;
+
+            case LEX_READ: // TODO add string support
+                int k;
+                from_st (args, i);
+                if (TID[i].get_type() == LEX_INT) {
+                    cout << "Input int value for" << TID[i].get_name() << endl;
+                    cin >> k;
+                    TID[i].put_value(k);
+                } else if (TID[i].get_type() == LEX_STRING) {
+                    string s;
+                    cout << "Input string for" << TID[i].get_name() << endl;
+                    cin >> s;
+                    TSTR[TID[i].get_value()] = s;
+                } else {
+                    string j;
+                    while (1) {
+                        cout << "Input boolean value (true or false) for" << TID[i].get_name() << endl;
+                        cin >> j;
+                        if (j != "true" && j != "false") {
+                            cout << "Error in input:true/false" << endl;
+                            continue;
+                        }
+                        k = (j == "true") ? 1 : 0;
+                        break;
+                    }
+                    TID[i].put_value(k);
+                }
+                TID[i].put_assign();
+                break;
+
+            case LEX_PLUS:
+                from_st (args, i);
+                from_st (args, j);
+                args.push (i + j);
+                break;
+
+            case LEX_SPLUS:
+                from_st (args, i);
+                from_st (args, j);
+                args.push(TSTR.size());
+                TSTR.push_back(TSTR[i] + TSTR[j]);
+                break;
+
+            case LEX_TIMES:
+                from_st (args, i);
+                from_st (args, j);
+                args.push (i * j);
+                break;
+
+            case LEX_MINUS:
+                from_st (args, i);
+                from_st (args, j);
+                args.push (j - i);
+                break;
+
+            case LEX_SLASH:
+                from_st ( args, i );
+                from_st ( args, j );
+                if (!i) {
+                    args.push ( j / i );
+                    break;
+                }
+                else
+                    throw "POLIZ:divide by zero";
+
+            case LEX_EQ:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( i == j );
+                break;
+
+            case LEX_LSS:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( j < i );
+                break;
+
+            case LEX_GTR:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( j > i );
+                break;
+
+            case LEX_LEQ:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( j <= i );
+                break;
+
+            case LEX_GEQ:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( j >= i );
+                break;
+
+            case LEX_NEQ:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( j != i );
+                break;
+
+            case LEX_SEQ:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( i == j );
+                break;
+
+            case LEX_SLSS:
+                from_st ( args, i );
+                from_st ( args, j );
+                args.push ( j < i );
+                break;
+
+            case LEX_SGTR:
+                from_st(args, i);
+                from_st(args, j);
+                args.push ( j > i );
+                break;
+
+            case LEX_SNEQ:
+                from_st(args, i);
+                from_st(args, j);
+                args.push (TSTR[j] != TSTR[i]);
+                break;
+
+            case LEX_ASSIGN:
+                from_st(args, i);
+                from_st(args, j);
+                TID[j].put_value(i);
+                TID[j].put_assign();
+                args.push(j);
+                break;
+
+            case LEX_SASSIGN:
+                from_st(args, i);
+                from_st(args, j);
+                TSTR[TID[j].get_value()] = TSTR[i];
+                TID[j].put_assign();
+                args.push(TID[j].get_value());
+                break;
+
+            case LEX_SEMICOLON:
+                from_st(args, j);
+                break;
+
+            default:
+                cout << pc_el << endl;
+                throw "POLIZ: unexpected elem";
+        }//end of switch
+        ++index;
+    };//end of while
+    cout << "Finish of executing!!!" << endl;
+}
+
+class Interpretator {
+    Parser   pars;
+    Executer E;
+public:
+    Interpretator ( const char* program ): pars (program) {}
+    void     interpretation ();
+};
+
+void Interpretator::interpretation () {
+    pars.analyze ();
+    E.execute ( pars.poliz );
+}
+
+#endif //MLANG_INTERPRETER_EXECUTER_H
